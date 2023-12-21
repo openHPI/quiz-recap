5db7593fe610561ea59d6a3b520553f9b4104cb9
Christoph Blessing <christophblessing@users.noreply.github.com>
2023-12-21 14:23:58 +0100

    please linter

--- src/components/Ansers.test.tsx
+++ src/components/Ansers.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { QuestionTypes } from '../types';
 import Answers from './Answers';
 

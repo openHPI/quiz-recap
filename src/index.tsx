113784a9e69f5acc2967649c88143a2a559d68bb
Christoph Blessing <christophblessing@users.noreply.github.com>
2023-12-21 14:23:58 +0100

    add AI dataset

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { webtechData } from './static/data';
+import { aiData as data } from './static/data';
 
 const root = ReactDOM.createRoot(
   document.getElementById('quiz-recap') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App questions={webtechData.questions} answers={webtechData.answers} />
+    <App questions={data.questions} answers={data.answers} />
   </React.StrictMode>
 );

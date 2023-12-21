393a70a84f3339f6e59d5c34ded21ab7c2c339bd
Christoph Blessing <christophblessing@users.noreply.github.com>
2023-12-21 14:23:56 +0100

    Start implementing quiz end

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Form from './components/Form';
+import Result from './components/Result';
 import { Data, Question, Answer } from './types';
 
 function App(data: Data) {
   const [question, setQuestion] = useState<Question>();
   const [answers, setAnswers] = useState<Answer[]>();
   const [questionIndex, setQuestionIndex] = useState(0);
+  const [quizEnded, setQuizEnded] = useState(false);
 
   const getAnswers = (question: Question, allAnswers: Answer[]): Answer[] => {
     return question.answers.map((answerId) => {
@@ -20,22 +22,34 @@ function App(data: Data) {
   }, [data, questionIndex]);
 
   const nextQuestion = () => {
-    const nextQuestionIndex = questionIndex + 1;
-    setQuestionIndex(nextQuestionIndex);
+    if (questionIndex <= data.questions.length) {
+      const nextQuestionIndex = questionIndex + 1;
+      setQuestionIndex(nextQuestionIndex);
+    } else {
+      setQuizEnded(true);
+    }
+  };
+
+  const handleEndQuiz = () => {
+    setQuizEnded(true);
   };
 
   return (
     <div className="App">
       <h1>Quiz recap</h1>
-      {question && answers ? (
-        <Form
-          question={question}
-          answers={answers}
-          nextQuestion={nextQuestion}
-        ></Form>
-      ) : (
-        '...Loading'
+      {question && answers && !quizEnded && (
+        <div>
+          <Form
+            question={question}
+            answers={answers}
+            nextQuestion={nextQuestion}
+          ></Form>
+          <button type="button" onClick={handleEndQuiz}>
+            End Quiz
+          </button>
+        </div>
       )}
+      {quizEnded && <Result></Result>}
     </div>
   );
 }
---
+++ src/components/Result.tsx
@@ -0,0 +1,9 @@
+const Result = () => {
+  return (
+    <div>
+      <h2>Final Result</h2>
+    </div>
+  );
+};
+
+export default Result;

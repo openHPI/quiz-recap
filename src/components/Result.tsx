d2c5f5d434b4b64bfcb284d868e1a33b110d7498
Christoph Blessing <christophblessing@users.noreply.github.com>
2023-12-21 14:23:58 +0100

    Refine results page

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -15,34 +15,36 @@ const Result = ({
   );
   return (
     <div>
-      <h2>Result</h2>
-      <table>
-        <caption>
-          {results.length
-            ? `You answered ${correctAnswers} of ${totalQuestions} correctly.`
-            : 'You did not answer any questions'}
-        </caption>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Question</th>
-            {/* <th>Attempts</th>
+      <h3>Result</h3>
+      {results.length ? (
+        <table>
+          <caption>
+            You answered {correctAnswers} of {totalQuestions} correctly.
+          </caption>
+          <thead>
+            <tr>
+              <th aria-label="Correctly answered"></th>
+              <th>Question</th>
+              {/* <th>Attempts</th>
             <th>Link</th> */}
-          </tr>
-        </thead>
-        <tbody>
-          {results.map((result: ResultType) => {
-            return (
-              <tr key={result.question.id}>
-                <td>{result.correctlyAnswered ? '✅' : '❌'}</td>
-                <td>{result.question.text}</td>
-                {/* <td>{result.attempts}</td>
+            </tr>
+          </thead>
+          <tbody>
+            {results.map((result: ResultType) => {
+              return (
+                <tr key={result.question.id}>
+                  <td>{result.correctlyAnswered ? '✅' : '❌'}</td>
+                  <td>{result.question.text}</td>
+                  {/* <td>{result.attempts}</td>
                 <td>{result.link}</td> */}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : (
+        <p>You did not answer any questions.</p>
+      )}
     </div>
   );
 };

45f26af17ddb564be1692214d392ab3e8ff75b26
Christoph Blessing <christophblessing@users.noreply.github.com>
2023-12-21 14:23:55 +0100

    Add basic specs

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Quiz recap title', () => {
+test('renders Quiz recap components', async () => {
   render(<App />);
+
   const titleElement = screen.getByText(/Quiz recap/i);
+  const submitButton = screen.getByRole('button');
+  const answers = await screen.findAllByRole('checkbox');
+  // ToDo: Load testing dataset instead
+  const question = screen.getByText(/Was zeigt uns ein Histogramm an?/i);
+
   expect(titleElement).toBeInTheDocument();
+
+  expect(submitButton).toBeInTheDocument();
+  expect(submitButton).not.toBeDisabled();
+
+  expect(answers.length).toBeGreaterThanOrEqual(1);
+  expect(question).toBeInTheDocument();
 });

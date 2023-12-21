f2baa13ebe455828cf0da94bf085c8ebcfd91d6f
Christoph Blessing <christophblessing@users.noreply.github.com>
2023-12-21 14:23:59 +0100

    Remove over-nesting in App component

--- src/App.tsx
+++ src/App.tsx
@@ -30,68 +30,64 @@ function App(data: Data) {
     >
       <div className="bg-gray-100 rounded p-5">
         <h2 className="text-xl pb-4">Quiz recap</h2>
-        <div className="">
-          {quizStarted ? (
-            <>
-              <Quiz data={data} />
-            </>
-          ) : (
-            <div>
-              <h3 className="text-lg pb-2">
-                Here you can practice your knowledge for the course!
-              </h3>
+        {quizStarted ? (
+          <Quiz data={data} />
+        ) : (
+          <>
+            <h3 className="text-lg pb-2">
+              Here you can practice your knowledge for the course!
+            </h3>
 
-              <p className="pb-2">
-                After you decide for a quiz size you will get a random set of
-                questions. Questions with one correct answer are indicated by a
-                radio button. Those with multiple correct answers are indicated
-                by a checkbox.
-              </p>
-              <p className="pb-2">
-                Choose a quiz type depending on how many questions you want to
-                practice now:
-              </p>
-              <ul>
-                <li>
-                  <button
-                    type="button"
-                    className="bg-blue-300 rounded p-2 m-2 w-full hover:bg-blue-500 active:bg-blue-500 focus:bg-blue-500"
-                    onClick={() => {
-                      setNumberOfQuestions(completeSet);
-                      setQuizStarted(true);
-                    }}
-                  >
-                    Complete set (all {completeSet} questions)
-                  </button>
-                </li>
-                <li>
-                  <button
-                    type="button"
-                    className="bg-blue-300 rounded p-2 m-2 w-full hover:bg-blue-500 active:bg-blue-500 focus:bg-blue-500"
-                    onClick={() => {
-                      setNumberOfQuestions(mediumSet);
-                      setQuizStarted(true);
-                    }}
-                  >
-                    Medium set ({mediumSet} questions)
-                  </button>
-                </li>
-                <li>
-                  <button
-                    type="button"
-                    className="bg-blue-300 rounded p-2 m-2 w-full hover:bg-blue-500 active:bg-blue-500 focus:bg-blue-500"
-                    onClick={() => {
-                      setNumberOfQuestions(quickSet);
-                      setQuizStarted(true);
-                    }}
-                  >
-                    Quick set ({quickSet} questions)
-                  </button>
-                </li>
-              </ul>
-            </div>
-          )}
-        </div>
+            <p className="pb-2">
+              After you decide for a quiz size you will get a random set of
+              questions. Questions with one correct answer are indicated by a
+              radio button. Those with multiple correct answers are indicated by
+              a checkbox.
+            </p>
+            <p className="pb-4">
+              Choose a quiz type below depending on how many questions you want
+              to practice.
+            </p>
+            <ul>
+              <li>
+                <button
+                  type="button"
+                  className="bg-blue-300 rounded p-2 m-2 w-full hover:bg-blue-500 active:bg-blue-500 focus:bg-blue-500"
+                  onClick={() => {
+                    setNumberOfQuestions(completeSet);
+                    setQuizStarted(true);
+                  }}
+                >
+                  Complete set (all {completeSet} questions)
+                </button>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  className="bg-blue-300 rounded p-2 m-2 w-full hover:bg-blue-500 active:bg-blue-500 focus:bg-blue-500"
+                  onClick={() => {
+                    setNumberOfQuestions(mediumSet);
+                    setQuizStarted(true);
+                  }}
+                >
+                  Medium set ({mediumSet} questions)
+                </button>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  className="bg-blue-300 rounded p-2 m-2 w-full hover:bg-blue-500 active:bg-blue-500 focus:bg-blue-500"
+                  onClick={() => {
+                    setNumberOfQuestions(quickSet);
+                    setQuizStarted(true);
+                  }}
+                >
+                  Quick set ({quickSet} questions)
+                </button>
+              </li>
+            </ul>
+          </>
+        )}
       </div>
     </Context.Provider>
   );

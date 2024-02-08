e95aa5358697f310072121fb6c7e065e03544e51
Christoph Blessing <christophblessing@users.noreply.github.com>
2024-02-08 12:02:55 +0100

    Vitest setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./vitest-setup.js'],
+  },
 });

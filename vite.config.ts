5e7773df8c2bc97212e0cb13b255d959b2124677
Christoph Blessing <christophblessing@users.noreply.github.com>
2024-02-14 10:17:19 +0100

    Export types in lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.tsx'),

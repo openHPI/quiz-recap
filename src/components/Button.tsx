8fd653ed080c150909a6acd97000f12e76d09131
Christoph Blessing <christophblessing@users.noreply.github.com>
2024-03-08 10:34:23 +0100

    Migrate Button styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,5 @@
+import styles from './Button.module.scss';
+
 const Button = ({
   text,
   onClickAction,
@@ -12,23 +14,15 @@ const Button = ({
   additionalClasses?: string;
 }) => {
   const classesBasedOnType = {
-    primary:
-      'qr-bg-primary hover:qr-bg-primary-dark focus:qr-bg-primary-dark active:qr-bg-primary-dark ',
-    neutral:
-      'qr-bg-neutral hover:qr-bg-neutral-dark focus:qr-bg-neutral-dark active:qr-bg-neutral-dark ',
-    success:
-      'qr-bg-success hover:qr-bg-success-dark focus:qr-bg-success-dark active:qr-bg-success-dark ',
-    danger:
-      'qr-bg-danger hover:qr-bg-danger-dark focus:qr-bg-danger-dark active:qr-bg-danger-dark ',
+    primary: styles.primary,
+    neutral: styles.neutral,
+    success: styles.success,
+    danger: styles.danger,
   };
   return (
     <button
       type={type}
-      className={
-        'qr-rounded qr-p-2 qr-text-white ' +
-        classesBasedOnType[style] +
-        additionalClasses
-      }
+      className={`${styles.button} + ${classesBasedOnType[style]} + ${additionalClasses}`}
       onClick={() => {
         onClickAction && onClickAction();
       }}

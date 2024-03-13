bfa4e83779e2cd73ed8d2822af6ccedd89523e39
Julia Casamitjana <62883011+JuliaCasamitjana@users.noreply.github.com>
2024-03-13 16:48:23 +0100

    Adjust test setup to use locales
    If the test needs the locales the render method uses
    the wrapper option to provide the i18n test setup
    
    reference: https://react.i18next.com/misc/testing#testing-without-stubbing
    
    Part of XI-6319

--- src/util.tsx
+++ src/util.tsx
@@ -1,4 +1,6 @@
 import { AnswerType, Data } from './types';
+import i18n from './i18n';
+import { I18nextProvider } from 'react-i18next';
 
 export const validateSelectionIsCorrect = (
   answers: AnswerType[],
@@ -32,4 +34,13 @@ export const isAlreadySelected = (
 
 export const getRandomSet = (set: Data, count: number): Data => {
   return set.sort(() => 0.5 - Math.random()).slice(0, count);
+};
+
+/**
+ * Test Helpers
+ **/
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const translationsProvider = ({ children }: any) => {
+  return <I18nextProvider i18n={i18n}>{children} </I18nextProvider>;
 };
